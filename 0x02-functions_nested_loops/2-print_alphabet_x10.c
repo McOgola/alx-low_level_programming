f2157112121a86c873c19d93bf6008131da5efe0
McOgola <omondiogola69@gmail.com>
2023-09-13 06:32:32 -0700

    Prints alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet_x10 - print alphabet 10 times
  *
  *
- * Return: Always 0 (success)
+ * Return: void here
  *
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10.c();
-	return (0);
+	int i;
+	int j;
+
+	for (j = 1; i <= 10, i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n);
+	}
 }
