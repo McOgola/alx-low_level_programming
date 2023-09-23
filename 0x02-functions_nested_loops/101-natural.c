0b283e3ceb4009334bf6960480ef20afb12ce13a
McOgola <omondiogola69@gmail.com>
2023-09-23 02:44:05 -0700

    Multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints all multiples of 3 and 5
+ * Return:  sum of the multiples of 3 and 5 except 1024
+ */
+int main(sum)
+{
+	int sum;
+
+	sum_of_special numbers = 0
+	{
+		for (1, 1024)
+		{
+			
+		if (number % 3 == 0 or number % 5 == 0);
+		{
+		_putchar(sum_of_special_numbers('\n');
+		}
+		return (sum_of_special_numbers);
+		}
+	}
+}
