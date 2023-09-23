4a728c12e94be862cd2ef350c326eea337539bdb
McOgola <omondiogola69@gmail.com>
2023-09-23 07:17:09 -0700

    Print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,13 +8,9 @@ void print_numbers(void)
 {
 	char c;
 
-	int i = 0
-		while (i <= 9)
-		{
-			for (i = '0'; i <= '9'; i++)
-			{
-				_putchar(c);
-			}
-			_putchar('\n');
-		}
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
