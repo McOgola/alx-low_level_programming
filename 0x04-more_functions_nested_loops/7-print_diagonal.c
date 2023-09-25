72b2c67ae62d12c3dc768f90d12615d356c6ddcf
McOgola <omondiogola69@gmail.com>
2023-09-25 16:46:09 -0700

    Draws a diagnol line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagram line on the terminal
+ * @n: the number of times the special character \ should be printed
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					putchar('\\');
+				else if (j < i)
+					putchar(' ');
+			}
+			putchar('\n');
+		}
+	}
+}
