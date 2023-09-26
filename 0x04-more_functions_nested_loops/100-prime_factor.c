6cd81970c5d587555a874eafc60ebecdc4fd6d85
McOgola <omondiogola69@gmail.com>
2023-09-25 17:46:49 -0700

    Distinguish prime numbers from composite numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -17,9 +17,9 @@ int main(void)
 	while (n % 2 == 0)
 	{
 		max = 2;
-		n / = 2;
+		n /= 2;
 	}
-	for (i = 3; i <= sart(n); i = i + 2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
