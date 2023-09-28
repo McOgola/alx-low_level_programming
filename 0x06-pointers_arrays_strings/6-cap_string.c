fef449a1edca42094f2d5208d411eb6dde79d137
McOgola <omondiogola69@gmail.com>
2023-09-28 12:30:35 -0700

    Capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * *cap_string - Capitalizes all word of a string
+ * *cap_string - Capitalizes all words of a string
  * @str: The string to be capitalized
  * Return: A pointer to the changed string
  */
-char *cap_string(char *str)
+char *cap_string(char *)
 {
 	int index = 0;
 
